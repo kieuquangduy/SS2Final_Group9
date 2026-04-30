45f7275cfee48346ae6d77cae757ac975861ea75
Hoàng <Sheepishlysheeping@gmail.com>
2026-04-30 20:49:24 +0700

    feat/toggle visuals

---
+++ frontend/app/components/common/page/toggle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex gap-4">
+    <p>{{ label }}:</p>
+    <div class="relative">
+      <UButton
+        v-for="option in options"
+        :key="option.hash"
+        class="absolute h-6 w-24 justify-center duration-200 transition-all"
+        :class="route.hash === option.hash ? 'scale-120 translate-x-0' : 'translate-x-[120%] opacity-50'"
+        :to="{ hash: option.hash }"
+        :label="option.label"
+        size="sm"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  label: string
+  options: {
+    label: string
+    hash: string
+  }[]
+}>()
+
+const route = useRoute()
+</script>
--- frontend/app/pages/dashboard/admin/manage-applications/index.vue
+++ frontend/app/pages/dashboard/admin/manage-applications/index.vue
@@ -1,61 +1,86 @@
 <template>
-    <div class="flex flex-col gap-10">
-        <CommonPageSection title="Manage Applications">
-            <div class="flex gap-8">
-                <div class="flex items-center gap-4">
-                    <p>Sort by:</p>
-                    <UButton label="Students" />
-                    <UButton label="Scholarships" />
-                </div>
-            </div>
-        </CommonPageSection>
-        <CommonPageSection v-if="data && data.length">
-            <UTable :columns="columns" :data="data" class="w-full">
-                <template #index-cell="{ row }">
-                    <span class="text-gray-500 font-medium">{{ row.index + 1 }}</span>
-                </template>
+  <div class="flex flex-col gap-10">
+    <CommonPageSection title="Manage Applications">
+      <div class="flex gap-8">
+        <div class="flex items-center gap-4">
+          <CommonPageToggle
+            label="Sort By"
+            :options="sortOptions"
+          />
+        </div>
+      </div>
+    </CommonPageSection>
+    <CommonPageSection v-if="data && data.length">
+      <UTable
+        :columns="columns"
+        :data="data"
+        class="w-full"
+      >
+        <template #index-cell="{ row }">
+          <span class="text-gray-500 font-medium">{{ row.index + 1 }}</span>
+        </template>
 
-                <template #icon-cell="{ row }">
-                    <div v-if="row.original.icon">
-                        <NuxtImg :src="row.original.icon" class="size-8" />
-                    </div>
-                </template>
+        <template #icon-cell="{ row }">
+          <div v-if="row.original.icon">
+            <NuxtImg
+              :src="row.original.icon"
+              class="size-8"
+            />
+          </div>
+        </template>
 
-                <template #actions-cell="{ row }">
-                    <div class="flex justify-end">
-                        <UButton icon="i-heroicons-eye" color="info" label="Select" class="cursor-pointer" @click="() => { navigateTo(`manage-applications/${row.original.id}`)}" />
-                    </div>
-                </template>
-            </UTable>
-        </CommonPageSection>
-        <CommonPageEmpty v-else />
-    </div>
+        <template #actions-cell="{ row }">
+          <div class="flex justify-end">
+            <UButton
+              icon="i-heroicons-eye"
+              color="info"
+              label="Select"
+              class="cursor-pointer"
+              @click="() => { navigateTo(`manage-applications/${row.original.id}`) }"
+            />
+          </div>
+        </template>
+      </UTable>
+    </CommonPageSection>
+    <CommonPageEmpty v-else />
+  </div>
 </template>
-<script setup lang="ts">
-import { useScholarshipList } from '~/composables/scholarship/useScholarshipList';
 
+<script setup lang="ts">
+import { useScholarshipList } from '~/composables/scholarship/useScholarshipList'
+
+const sortOptions = [
+  {
+    label: 'Applicants',
+    hash: '#applicants',
+  },
+  {
+    label: 'Scholarships',
+    hash: '#scholarships',
+  },
+]
 
 const { data } = await useScholarshipList()
 
 const columns: TableColumn[] = [
-    {
-        id: 'index',
-        header: '#'
-    },
-    {
-        id: 'icon',
-    },
-    {
-        accessorKey: 'title',
-        header: 'Scholarship Name'
-    },
-    {
-        accessorKey: 'deadline',
-        header: 'Deadline'
-    },
-    {
-        id: 'actions',
-        header: ''
-    }
+  {
+    id: 'index',
+    header: '#',
+  },
+  {
+    id: 'icon',
+  },
+  {
+    accessorKey: 'title',
+    header: 'Scholarship Name',
+  },
+  {
+    accessorKey: 'deadline',
+    header: 'Deadline',
+  },
+  {
+    id: 'actions',
+    header: '',
+  },
 ]
 </script>
