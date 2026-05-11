b30d8d676daae6a6a6a47827323058ff49076976
Hoàng <Sheepishlysheeping@gmail.com>
2026-05-11 20:38:43 +0700

    feat/chatbox ui

---
+++ frontend/app/components/common/chatbot/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="absolute w-140 pl-4">
+    <div class="animate-scoped-gradient bg-gradient-to-r from-info-400 via-purple-400 to-info-500 bg-[length:300%_100%] rounded-lg w-full relative px-2 flex flex-col ring-4 ring-purple-600">
+      <div class="absolute bottom-2 -left-2 size-4 bg-purple-600 rotate-45 rounded-sm -z-1" />
+      <div class="h-12 flex items-center">
+        <p class="font-bold text-white text-xl">
+          Hub Assistant
+        </p>
+        <UButton
+          icon="i-heroicons-x-mark-solid"
+          color="info"
+          variant="solid"
+          class="ml-auto cursor-pointer bg-white hover:bg-neutral-200"
+          :ui="{ base: 'p-0', leadingIcon: 'bg-purple-600 size-8' }"
+          @click="() => {}"
+        />
+      </div>
+      <div class="bg-white w-full h-80 rounded-lg p-2">
+        helooo
+      </div>
+      <div class=" flex h-14 items-center gap-4">
+        <input type="text" placeholder="Enter your message..." class="w-full bg-white rounded-lg p-2" />
+        <UButton
+          icon="i-heroicons-paper-airplane-solid"
+          color="info"
+          variant="solid"
+          class="ml-auto cursor-pointer bg-white hover:bg-neutral-200 size-10"
+          :ui="{ base: 'p-0', leadingIcon: 'bg-purple-600 mx-auto size-8' }"
+          @click="() => {}"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.animate-scoped-gradient {
+  background-size: 300% 100%;
+  animation: gradientMove 12s linear infinite;
+}
+
+@keyframes gradientMove {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+}
+</style>
--- frontend/app/components/common/sidebar/index.vue
+++ frontend/app/components/common/sidebar/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div :class="['h-screen transition-all', sidebarOpen ? 'w-50' : 'w-20']" />
     <div :class="['fixed h-screen top-0 left-0 p-2 transition-all z-98', sidebarOpen ? 'w-50' : 'w-20']">
-      <div class="size-full bg-info-300 flex flex-col gap-6 rounded-lg items-center shadow-xl overflow-hidden">
+      <div class="size-full bg-info-300 flex flex-col gap-6 rounded-lg items-center shadow-xl relative">
         <div class="w-full px-4">
           <NuxtImg
             :src="icon"
@@ -33,28 +33,35 @@
           </div>
         </div>
         <div
-          class="mt-auto mb-2 bg-red-500 w-full cursor-pointer"
+          class="mt-auto mb-2 w-full cursor-pointer"
         >
-          <div v-if="sidebarOpen" class="relative group flex items-center justify-center">
+          <div
+            v-if="sidebarOpen"
+            class="relative group flex items-center justify-center"
+          >
             <div class="size-12 z-2 rounded-full animate-scoped-gradient bg-gradient-to-r from-info-400 via-purple-400 to-info-500 bg-[length:300%_100%] p-2 shadow-xl transition-transform duration-400 ease-out group-hover:-translate-x-[80%]">
               <UIcon
                 name="i-heroicons-sparkles-solid"
                 class="size-full"
               />
             </div>
-            <div class="absolute bg-info-500 ring-2 ring-white rounded-lg opacity-0 group-hover:opacity-100 transition-all duration-300 ease-out group-hover:translate-x-[10%]">
+            <div class="absolute bg-purple-600 ring-2 ring-white rounded-lg opacity-0 group-hover:opacity-100 transition-all duration-300 ease-out group-hover:translate-x-[10%]">
               <p class="text-white p-1 pl-6">
                 Assistant
               </p>
             </div>
           </div>
-          <div v-else class="mx-auto size-12 z-2 rounded-full animate-scoped-gradient bg-gradient-to-r from-info-400 via-purple-400 to-info-500 bg-[length:300%_100%] p-2 shadow-xl">
+          <div
+            v-else
+            class="mx-auto size-12 z-2 rounded-full animate-scoped-gradient bg-gradient-to-r from-info-400 via-purple-400 to-info-500 bg-[length:300%_100%] p-2 shadow-xl"
+          >
             <UIcon
               name="i-heroicons-sparkles-solid"
               class="size-full"
             />
           </div>
         </div>
+        <CommonChatbot class="bottom-0 left-[100%] mb-2 ml-2" />
       </div>
       <div
         class="group absolute h-full w-2 py-2 right-0 top-0 cursor-pointer"
