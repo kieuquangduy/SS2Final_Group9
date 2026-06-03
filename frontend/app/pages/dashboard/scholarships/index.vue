8a45815d93923435186bd4b8d98424a3fcea5d2f
gdgochanu-vuhoang <Sheepishlysheeping@gmail.com>
2026-06-04 05:43:59 +0700

    fix/scholarship-list filter styling

--- frontend/app/pages/dashboard/scholarships/index.vue
+++ frontend/app/pages/dashboard/scholarships/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col gap-10">
-    <div class="relative flex justify-center">
-      <ScholarshipAnimatedBackground />
-      <div class="absolute z-2 -bottom-8 rounded-lg shadow-xl flex bg-white">
+    <ScholarshipAnimatedBackground />
+    <div class="sticky z-2 top-14 rounded-lg shadow-xl flex bg-white w-max mx-auto">
+      <div class="hidden md:flex">
         <UButton
           v-for="option in tierOptions"
           :key="option.label"
@@ -14,6 +14,18 @@
           @click="toggleTier(option.hash)"
         />
       </div>
+      <UDropdownMenu
+        class="md:hidden"
+        :items="tierOptions"
+      >
+        <UButton
+          icon="i-heroicons-bars-3"
+          :label="route.hash.slice(1).toUpperCase() || 'TIER'"
+          color="info"
+          variant="solid"
+          class="w-48 h-16 rounded-lg font-bold text-xl justify-center"
+        />
+      </UDropdownMenu>
     </div>
     <CommonPageSection
       v-if="scholarships?.length"
@@ -40,24 +52,27 @@ import { useScholarshipList } from '~/composables/scholarship/useScholarshipList
 const route = useRoute()
 const router = useRouter()
 
-const { all, fetchPage, isLoading, canLoadMore, filterByTier } = await useScholarshipList({limit: 3}) 
+const { all, fetchPage, isLoading, canLoadMore, filterByTier } = await useScholarshipList({ limit: 3 })
 const scholarships = computed(() => {
   if (route.hash) return filterByTier(all.value, route.hash.slice(1))
   return all.value
 })
 
-const tierOptions = ref([
+const tierOptions = computed(() => [
   {
     label: 'Gold',
     hash: '#gold',
+    onClick: () => toggleTier('#gold'),
   },
   {
     label: 'Silver',
     hash: '#silver',
+    onClick: () => toggleTier('#silver'),
   },
   {
     label: 'Venue',
     hash: '#venue',
+    onClick: () => toggleTier('#venue'),
   },
 ])
 
